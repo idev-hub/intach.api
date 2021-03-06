1b1c38ebc3496a9af96ef52393841472ebc6e215
Idev <idev.hub@ya.ru>
2021-03-07 01:38:55 +0500

    fix verstion nodejs

--- config/database.ts
+++ config/database.ts
@@ -11,29 +11,29 @@ import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
   /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
+   |--------------------------------------------------------------------------
+   | Connection
+   |--------------------------------------------------------------------------
+   |
+   | The primary connection for making database queries across the application
+   | You can use any key from the `connections` object defined in this same
+   | file.
+   |
+   */
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
     /*
-    |--------------------------------------------------------------------------
-    | PostgreSQL config
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for PostgreSQL database. Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i pg
-    |
-    */
+     |--------------------------------------------------------------------------
+     | PostgreSQL config
+     |--------------------------------------------------------------------------
+     |
+     | Configuration for PostgreSQL database. Make sure to install the driver
+     | from npm when using this connection
+     |
+     | npm i pg
+     |
+     */
     pg: {
       client: 'pg',
       connection: {
@@ -42,28 +42,29 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: !!(Env.get('PG_DB_SSL'))
+        ssl: (Env.get('PG_USER') === 'development') ? false : {
+          rejectUnauthorized: false
+        }
       },
       healthCheck: false,
-			debug: false,
+      debug: false,
     },
 
   },
 
   /*
-  |--------------------------------------------------------------------------
-  | ORM Configuration
-  |--------------------------------------------------------------------------
-  |
-  | Following are some of the configuration options to tweak the conventional
-  | settings of the ORM. For example:
-  |
-  | - Define a custom function to compute the default table name for a given model.
-  | - Or define a custom function to compute the primary key for a given model.
-  |
-  */
-  orm: {
-  },
+   |--------------------------------------------------------------------------
+   | ORM Configuration
+   |--------------------------------------------------------------------------
+   |
+   | Following are some of the configuration options to tweak the conventional
+   | settings of the ORM. For example:
+   |
+   | - Define a custom function to compute the default table name for a given model.
+   | - Or define a custom function to compute the primary key for a given model.
+   |
+   */
+  orm: {},
 }
 
 export default databaseConfig
