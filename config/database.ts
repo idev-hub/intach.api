a7ba9f44e34dbd423cd129374f4b4f18367f7690
Idev <idev.hub@ya.ru>
2021-03-07 01:30:43 +0500

    fix verstion nodejs

--- config/database.ts
+++ config/database.ts
@@ -42,7 +42,7 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: Env.get('PG_DB_SSL') || false
+        ssl: !!(Env.get('PG_DB_SSL'))
       },
       healthCheck: false,
 			debug: false,
